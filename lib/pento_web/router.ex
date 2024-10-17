1a28203dcdb9594c2ceac18fc543a624f0be5104
kuatroka <aaarmani_2000@yahoo.com>
2024-10-18 00:38:54 +0100

    had to add 'config :bcrypt_elixir, :log_rounds, 12' inside 'config.exe' and recompile to make auth work

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -22,54 +22,54 @@ defmodule PentoWeb.Router do
 
     get "/", PageController, :home
 
-    live "/guess", WrongLive
   end
 
   # Other scopes may use custom stacks.
   # scope "/api", PentoWeb do
-  #   pipe_through :api
-  # end
+    #   pipe_through :api
+    # end
 
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:pento, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
+    # Enable LiveDashboard and Swoosh mailbox preview in development
+    if Application.compile_env(:pento, :dev_routes) do
+      # If you want to use the LiveDashboard in production, you should put
+      # it behind authentication and allow only admins to access it.
+      # If your application does not have an admins-only section yet,
+      # you can use Plug.BasicAuth to set up some basic authentication
+      # as long as you are also using SSL (which you should anyway).
+      import Phoenix.LiveDashboard.Router
 
-    scope "/dev" do
-      pipe_through :browser
+      scope "/dev" do
+        pipe_through :browser
 
-      live_dashboard "/dashboard", metrics: PentoWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+        live_dashboard "/dashboard", metrics: PentoWeb.Telemetry
+        forward "/mailbox", Plug.Swoosh.MailboxPreview
+      end
     end
-  end
 
-  ## Authentication routes
+    ## Authentication routes
 
-  scope "/", PentoWeb do
-    pipe_through [:browser, :redirect_if_user_is_authenticated]
+    scope "/", PentoWeb do
+      pipe_through [:browser, :redirect_if_user_is_authenticated]
 
-    live_session :redirect_if_user_is_authenticated,
+      live_session :redirect_if_user_is_authenticated,
       on_mount: [{PentoWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
-      live "/users/log_in", UserLoginLive, :new
-      live "/users/reset_password", UserForgotPasswordLive, :new
-      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+        live "/users/register", UserRegistrationLive, :new
+        live "/users/log_in", UserLoginLive, :new
+        live "/users/reset_password", UserForgotPasswordLive, :new
+        live "/users/reset_password/:token", UserResetPasswordLive, :edit
+      end
+
+      post "/users/log_in", UserSessionController, :create
     end
 
-    post "/users/log_in", UserSessionController, :create
-  end
+    scope "/", PentoWeb do
+      pipe_through [:browser, :require_authenticated_user]
 
-  scope "/", PentoWeb do
-    pipe_through [:browser, :require_authenticated_user]
-
-    live_session :require_authenticated_user,
+      live_session :require_authenticated_user,
       on_mount: [{PentoWeb.UserAuth, :ensure_authenticated}] do
-      live "/users/settings", UserSettingsLive, :edit
-      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+        live "/users/settings", UserSettingsLive, :edit
+        live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+        live "/guess", WrongLive
     end
   end
 
