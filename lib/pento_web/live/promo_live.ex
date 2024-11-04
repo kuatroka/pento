6e7455199fadce413f8e8e598c31fd2eb4663c1b
kuatroka <aaarmani_2000@yahoo.com>
2024-11-04 21:18:04 +0000

    feat: Add recipient form to promo live view

--- lib/pento_web/live/promo_live.ex
+++ lib/pento_web/live/promo_live.ex
@@ -4,6 +4,38 @@ defmodule PentoWeb.PromoLive do
   alias Pento.Promo.Recipient
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+    socket
+    |> assign_recipient()
+    |> assign_changeset()
+    }
   end
+
+  def assign_recipient(socket) do
+    socket
+    |> assign(:recipient, %Recipient{})
+  end
+
+  def assign_changeset(%{assigns: %{recipient: recipient}} = socket) do
+    changeset = Promo.change_recipient(recipient)
+    form = to_form(changeset)
+    socket
+    |> assign(:changeset, changeset)
+    |> assign(:form, form)
+  end
+
+  def handle_event("validate",
+      %{"recipient" => recipient_params},
+      %{assigns: %{recipient: recipient}} = socket) do
+      changeset =
+        recipient
+        |> Promo.change_recipient(recipient_params)
+        |> Map.put(:action, :validate)
+      form = to_form(changeset)
+      {:noreply, assign(socket, changeset: changeset, form: form)}
+
+  end
+
+
+
 end
